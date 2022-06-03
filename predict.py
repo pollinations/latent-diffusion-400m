194379f05602b6b9c00eb5d64f349b5e10ef2095
Niels Warncke <niels.warncke@gmail.com>
2022-06-03 11:14:20 +0200

    fix ci

--- predict.py
+++ predict.py
@@ -1,41 +1,36 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path
-from ldm.model import LDM
 import sys
+
+from cog import BasePredictor
+
 sys.path.append(".")
-sys.path.append('./taming-transformers')
-from taming.models import vqgan 
-import torch
-from omegaconf import OmegaConf
-
-from ldm.util import instantiate_from_config
-
-import argparse, os, sys, glob
-import torch
-import numpy as np
-from omegaconf import OmegaConf
-from PIL import Image
-from tqdm import tqdm, trange
-from einops import rearrange
-from torchvision.utils import make_grid
-import transformers
+sys.path.append("./taming-transformers")
+import argparse
 import gc
-from ldm.util import instantiate_from_config
+import os
+import sys
+
+import numpy as np
+import open_clip
+import torch
+from einops import rearrange
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
-from open_clip import tokenizer
-import open_clip
-import argparse
-
-import os
+from ldm.util import instantiate_from_config
+from omegaconf import OmegaConf
+from PIL import Image
+from torchvision.utils import make_grid
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        clip_model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='openai')
+        clip_model, _, preprocess = open_clip.create_model_and_transforms(
+            "ViT-B-32", pretrained="openai"
+        )
+
         def load_model_from_config(config, ckpt, verbose=False):
             print(f"Loading model from {ckpt}")
             pl_sd = torch.load(ckpt, map_location="cuda:0")
@@ -53,10 +48,12 @@ class Predictor(BasePredictor):
             model.eval()
             return model
 
-        config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml") 
-        model = load_model_from_config(config, "/content/models/ldm-model.ckpt") 
+        config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")
+        model = load_model_from_config(config, "/content/models/ldm-model.ckpt")
 
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
         self.model = model.to(device)
 
     def predict(
@@ -65,42 +62,44 @@ class Predictor(BasePredictor):
         # scale: float = Input(
         #     description="Factor to scale image by", ge=0, le=10, default=1.5
         # ),
-        Prompt = "Abstract geometric Chihuahua",
-        Steps = 100,
-        ETA = 1,
-        Iterations =  1,
+        Prompt="Abstract geometric Chihuahua",
+        Steps=100,
+        ETA=1,
+        Iterations=1,
         Samples_in_parallel=4,
         Diversity_scale=10.0,
         PLMS_sampling=True,
-        output_path = "/content",
+        output_path="/content",
         Width=256,
-        Height=256) -> None:
+        Height=256,
+    ) -> None:
         """Run a single prediction on the model"""
         Prompts = Prompt
         os.makedirs(output_path, exist_ok=True)
-        
+
         os.system(f"rm -rf /content/steps")
         os.system(f"mkdir -p /content/steps")
 
         frame_id = 0
+
         def save_img_callback(pred_x0, i):
             global frame_id
             # print(pred_x0)
             x_samples_ddim = self.model.decode_first_stage(pred_x0)
-            imgs = torch.clamp((x_samples_ddim+1.0)/2.0, min=0.0, max=1.0)
+            imgs = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
             grid = imgs
-            #grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+            # grid = rearrange(grid, 'n b c h w -> (n b) c h w')
             rows = len(imgs)
             # check if rows is quadratic and if yes take the square root
             height = int(rows**0.5)
             grid = make_grid(imgs, nrow=height)
             # to image
-            grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-            step_out = os.path.join("/content/steps", f'aaa_{frame_id:04}.png')
+            grid = 255.0 * rearrange(grid, "c h w -> h w c").cpu().numpy()
+            step_out = os.path.join("/content/steps", f"aaa_{frame_id:04}.png")
             Image.fromarray(grid.astype(np.uint8)).save(step_out)
 
             if frame_id % 10 == 0:
-                progress_out = os.path.join(output_path, "aaa_progress.png") 
+                progress_out = os.path.join(output_path, "aaa_progress.png")
                 Image.fromarray(grid.astype(np.uint8)).save(progress_out)
             frame_id += 1
 
@@ -112,71 +111,86 @@ class Predictor(BasePredictor):
                 sampler = PLMSSampler(self.model)
             else:
                 sampler = DDIMSampler(self.model)
-            
+
             os.makedirs(opt.outdir, exist_ok=True)
             outpath = opt.outdir
 
             sample_path = os.path.join(outpath, "samples")
             os.makedirs(sample_path, exist_ok=True)
-            base_count = len(os.listdir(sample_path))
 
-            all_samples=list()
+            all_samples = list()
             samples_ddim, x_samples_ddim = None, None
             with torch.no_grad():
                 with torch.cuda.amp.autocast():
                     with self.model.ema_scope():
                         uc = None
                         if opt.scale > 0:
-                            uc = self.model.get_learned_conditioning(opt.n_samples * [""])
+                            uc = self.model.get_learned_conditioning(
+                                opt.n_samples * [""]
+                            )
                         for prompt in opt.prompts:
                             print(prompt)
                             for n in range(opt.n_iter):
-                                c = self.model.get_learned_conditioning(opt.n_samples * [prompt])
-                                shape = [4, opt.H//8, opt.W//8]
-                                samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
-                                                                conditioning=c,
-                                                                batch_size=opt.n_samples,
-                                                                shape=shape,
-                                                                verbose=False,
-                                                                img_callback=save_img_callback,
-                                                                unconditional_guidance_scale=opt.scale,
-                                                                unconditional_conditioning=uc,
-                                                                eta=opt.ddim_eta,
-                                                                x_T=samples_ddim)
+                                c = self.model.get_learned_conditioning(
+                                    opt.n_samples * [prompt]
+                                )
+                                shape = [4, opt.H // 8, opt.W // 8]
+                                samples_ddim, _ = sampler.sample(
+                                    S=opt.ddim_steps,
+                                    conditioning=c,
+                                    batch_size=opt.n_samples,
+                                    shape=shape,
+                                    verbose=False,
+                                    img_callback=save_img_callback,
+                                    unconditional_guidance_scale=opt.scale,
+                                    unconditional_conditioning=uc,
+                                    eta=opt.ddim_eta,
+                                    x_T=samples_ddim,
+                                )
 
-                                x_samples_ddim = self.model.decode_first_stage(samples_ddim)
-                                x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0, min=0.0, max=1.0)
+                                x_samples_ddim = self.model.decode_first_stage(
+                                    samples_ddim
+                                )
+                                x_samples_ddim = torch.clamp(
+                                    (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0
+                                )
                                 all_samples.append(x_samples_ddim)
                 # additionally, save as grid
                 grid = torch.stack(all_samples, 0)
-                grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+                grid = rearrange(grid, "n b c h w -> (n b) c h w")
                 rows = opt.n_samples
                 # check if rows is quadratic and if yes take the square root
                 height = int(rows**0.5)
                 grid = make_grid(grid, nrow=height)
                 # to image
-                grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                #Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'zzz_{prompt.replace(" ", "-")}.png'))
+                grid = 255.0 * rearrange(grid, "c h w -> h w c").cpu().numpy()
+                # Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'zzz_{prompt.replace(" ", "-")}.png'))
                 # save individual images
-                for n,x_sample in enumerate(all_samples[0]):
+                for n, x_sample in enumerate(all_samples[0]):
                     x_sample = x_sample.squeeze()
-                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                    x_sample = 255.0 * rearrange(
+                        x_sample.cpu().numpy(), "c h w -> h w c"
+                    )
                     prompt_filename = prompt.replace(" ", "-")
-                    Image.fromarray(x_sample.astype(np.uint8)).save(os.path.join(output_path, f"{output_path}/yyy_{prompt_filename}_{n}.png"))
+                    Image.fromarray(x_sample.astype(np.uint8)).save(
+                        os.path.join(
+                            output_path, f"{output_path}/yyy_{prompt_filename}_{n}.png"
+                        )
+                    )
 
         os.system(f"rm {output_path}/aaa_*.png")
 
         args = argparse.Namespace(
-            prompts = Prompts.split("->"), 
+            prompts=Prompts.split("->"),
             outdir=output_path,
-            ddim_steps = Steps,
-            ddim_eta = ETA,
-            n_iter = Iterations,
+            ddim_steps=Steps,
+            ddim_eta=ETA,
+            n_iter=Iterations,
             W=Width,
             H=Height,
             n_samples=Samples_in_parallel,
             scale=Diversity_scale,
-            plms=PLMS_sampling
+            plms=PLMS_sampling,
         )
         run(args)
 
@@ -185,4 +199,6 @@ class Predictor(BasePredictor):
         # !cp -v $last_frame /content/steps/aaa_0000.png
         # !cp -v $last_frame /content/steps/aaa_0001.png
         encoding_options = "-c:v libx264 -crf 20 -preset slow -vf format=yuv420p -c:a aac -movflags +faststart"
-        os.system(f"ffmpeg -y -r 10 -i /content/steps/aaa_%04d.png {encoding_options} {output_path}/zzz_output.mp4")
+        os.system(
+            f"ffmpeg -y -r 10 -i /content/steps/aaa_%04d.png {encoding_options} {output_path}/zzz_output.mp4"
+        )
