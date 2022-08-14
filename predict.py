2da7126c0ed416dc95eb72992bb14737eb5164a6
Clay Mullis <claymullis@fastmail.com>
2022-08-14 22:18:38 +0000

    add three database backends

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ from typing import List, Optional
 import numpy as np
 import torch
 from clip_retrieval.clip_back import ParquetMetadataProvider, load_index, meta_to_dict
-from cog import BasePredictor, Input, Path, BaseModel
+from cog import BaseModel, BasePredictor, Input, Path
 from einops import rearrange, repeat
 from omegaconf import OmegaConf
 from PIL import Image
@@ -19,10 +19,11 @@ from torch import nn
 from ldm.models.diffusion.plms import PLMSSampler
 from ldm.util import instantiate_from_config
 
-class Generation(BaseModel):
-    """Helper Output class for CodeGen. Allows for output to be markdown file or string."""
-    generations: List[Path]
-    relevant_metadata: Optional[List]
+# class Generation(BaseModel):
+#     """Helper Output class for CodeGen. Allows for output to be markdown file or string."""
+#     generations: List[Path]
+#     relevant_metadata: Optional[List]
+
 
 @lru_cache(maxsize=None)  # cache the model, so we don't have to load it every time
 def load_clip(clip_model="ViT-L/14", use_jit=True, device="cpu"):
@@ -35,10 +36,11 @@ def encode_text_with_clip_model(
     text: str,
     clip_model: nn.Module,
     normalize: bool = True,
+    device: str = "cpu",
 ):
     assert text is not None and len(text) > 0, "must provide text"
-    tokens = clip.tokenize(text, truncate=True).to("cpu")
-    clip_text_embed = clip_model.encode_text(tokens).to("cpu")
+    tokens = clip.tokenize(text, truncate=True).to(device)
+    clip_text_embed = clip_model.encode_text(tokens).to(device)
     if normalize:
         clip_text_embed /= clip_text_embed.norm(dim=-1, keepdim=True)
     if clip_text_embed.ndim == 2:
@@ -67,15 +69,14 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
     model.eval()
+    print(f"Loaded model from {ckpt}")
     return model
 
 
-# '["similarity","hash","punsafe","pwatermark","aesthetic","LANGUAGE"]
 def map_to_metadata(
     indices, distances, num_images, metadata_provider, columns_to_return=["url"]
 ):
     results = []
-    # metas = metadata_provider.get(indices[:num_images], columns_to_return)
     metas = metadata_provider.get(indices[:num_images])
     for key, (dist, ind) in enumerate(zip(distances, indices)):
         output = {}
@@ -91,44 +92,48 @@ def map_to_metadata(
     return results
 
 
-# TODO
-image_index_path = "data/rdm/searchers/laion2B-en-aesthetic-index/image.index"
-parquet_folder = "data/rdm/searchers/laion2B-en-aesthetic-index/metadata/"
-
+def build_searcher(database_name: str):
+    image_index_path = Path(f"data/rdm/searchers/{database_name}/image.index")
+    assert image_index_path.exists(), f"database at {image_index_path} does not exist"
+    metadata_path = Path(f"data/rdm/searchers/{database_name}/metadata")
+    return {
+        "image_index": load_index(str(image_index_path), enable_faiss_memory_mapping=True),
+        "metadata_provider": ParquetMetadataProvider(str(metadata_path)) if metadata_path.exists() else None,
+    }
 
 class Predictor(BasePredictor):
-    def setup(self):
-        self.outdir = Path(tempfile.mkdtemp())
+    def __init__(self):
+        self.searchers = None
 
+    @torch.inference_mode()
+    def setup(self):
         self.device = (
             torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         )
-        print(f"Using device {self.device}")
-
-        self.clip_model, _ = load_clip("ViT-L/14", use_jit=False, device="cpu")
-        print(f"Loaded clip model ViT-L/14 to CPU")
-
-        self.image_index = load_index(
-            image_index_path, enable_faiss_memory_mapping=True
-        )
-        print(f"Loaded clip-retrieval faiss index at {image_index_path}")
-
-        self.metadata_provider = ParquetMetadataProvider(parquet_folder)
-
-        print(f"Loaded parquet metadata provider at {self.metadata_provider}")
 
         config = OmegaConf.load(f"configs/retrieval-augmented-diffusion/768x768.yaml")
         model = load_model_from_config(config, f"models/rdm/rdm768x768/model.ckpt")
         self.model = model.to(self.device)
-        print("Loaded latent-diffusion model.")
+        print(f"Loaded 1.4M param Retrieval Augmented Diffusion model to {self.device}")
+
+        use_jit = self.device.type.startswith("cuda")
+        self.clip_model, _ = load_clip("ViT-L/14", use_jit=use_jit, device=self.device)
+        print(f"Loaded clip model ViT-L/14 to {self.device} with use_jit={use_jit}")
 
         self.sampler = PLMSSampler(self.model)
         print("Using PLMS sampler")
 
-    def knn_search(self, query: torch.Tensor, num_results: int):
+        self.searchers = {}
+        self.searchers["laion-aesthetic"] = build_searcher("laion-aesthetic")
+        self.searchers["prompt-engineer"] = build_searcher("prompt-engineer")
+        self.searchers["simulacra"] = build_searcher("simulacra")
+
+    @torch.no_grad()
+    def knn_search(self, knn_index, query: torch.Tensor, num_results: int):
+        # TODO rewrite this method
         query = query.squeeze(0)
         query = query.cpu().detach().numpy().astype("float32")
-        distances, indices, embeddings = self.image_index.search_and_reconstruct(
+        distances, indices, embeddings = knn_index.search_and_reconstruct(
             query, num_results
         )
         results = indices[0]  # first element is a list of indices
@@ -154,39 +159,56 @@ class Predictor(BasePredictor):
         ),
         prompt_scale: float = Input(
             default=5.0,
-            description="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+            description="Determines influence of the prompt on the generated image. Going above 5.0 is likely to cause artifacting.",
         ),
-        num_lookup_images: int = Input(
-            default=20,
-            description="The number of included neighbors in the knn search",
+        database_name: str = Input(
+            default="laion-aesthetic",
+            description="Which database to use for the semantic search. Different databases have different capabilties.",
+            choices=[
+                "laion-aesthetic",
+                "simulacra",
+                "prompt-engineer"
+            ],
+        ),
+        num_database_reuslts: int = Input(
+            default=10,
+            description="The number of search results to guide the generation with. Using more will 'broaden' capabilities of the model at the risk of causing mode collapse or artifacting.",
             ge=1,
             le=20,
         ),
         num_generations: int = Input(
             default=4,
-            description="how many times to repeat the query",  # TODO
+            description="Number of images to generate. Using more will make generation take longer.",  # TODO
         ),
-        height: int = Input(default=768, description="image height, in pixel space"),
-        width: int = Input(default=768, description="image width, in pixel space"),
+        height: int = Input(default=768, description="Desired height of generated images."),
+        width: int = Input(default=768, description="Desired width of generated images."),
         steps: int = Input(
             default=50,
-            description="how many steps to run the model for",
+            description="How many steps to run the model for. Using more will make generation take longer. 50 tends to work well.",
         ),
-    ) -> Generation:
+    ) -> List[Path]:
+        self.outdir = Path(tempfile.mkdtemp())
+
         prompt_embedding = encode_text_with_clip_model(
-            text=prompt, clip_model=self.clip_model, normalize=True
+            text=prompt, clip_model=self.clip_model, normalize=True, device=self.device
         )
         knn_distances, knn_indices, knn_embeddings = self.knn_search(
-            prompt_embedding, num_lookup_images
+            self.searchers[database_name]["image_index"],
+            prompt_embedding,
+            num_database_reuslts,
         )
-
-        relevant_metadata = map_to_metadata(
-            indices=knn_indices,
-            distances=knn_distances,
-            num_images=num_lookup_images,
-            metadata_provider=self.metadata_provider,
-        )
-
+        if self.searchers[database_name]["metadata_provider"] is not None:
+            search_results = map_to_metadata(
+                indices=knn_indices,
+                distances=knn_distances,
+                num_images=num_database_reuslts,
+                metadata_provider=self.searchers[database_name]["metadata_provider"],
+            )
+            for search_result in search_results:
+                print("-----------------------------------------------------")
+                print(f"caption: {search_result['caption']}")
+                print(f"url: {search_result['url']}")
+                print("-----------------------------------------------------")
         sample_conditioning = torch.cat(
             [
                 prompt_embedding.to(self.device),
@@ -224,17 +246,10 @@ class Predictor(BasePredictor):
 
         generation_paths = []
         for idx, generation in enumerate(decoded_generations):
-            generation = 255.0 * rearrange(
-                generation.cpu().numpy(), "c h w -> h w c"
-            )
+            generation = 255.0 * rearrange(generation.cpu().numpy(), "c h w -> h w c")
             x_sample_target_path = self.outdir.joinpath(f"sample_{idx:03d}.png")
-            x_sample_target_path = f"sample_{idx:03d}.png"  # TODO
             pil_image = Image.fromarray(generation.astype(np.uint8))
             pil_image.save(x_sample_target_path, "png")
-            generation_paths.append(
-                Path(x_sample_target_path)
-            )
-        return Generation(
-            generations=generation_paths,
-            metadata=relevant_metadata,
-        )
+            pil_image.save(f"sample_{idx:03d}.png")
+            generation_paths.append(Path(x_sample_target_path))
+        return generation_paths
