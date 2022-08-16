919e5d9872defb293c8a33d775c06740a655ebeb
Niels Warncke <niels.warncke@gmail.com>
2022-08-16 11:48:50 +0200

    updated ci

--- .github/workflows/extract_openapi.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-with open("inspect.json", "r") as f:
-    data = json.load(f)[0]
-
-with open("openapi.json", "w") as f:
-   f.write(data["ContainerConfig"]["Labels"]["org.cogmodel.openapi_schema"])
